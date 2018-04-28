184f1649b21288cd8626d934884e25ab98783bd9
Logan Call <logancall.dev@gmail.com>
2018-04-28 14:32:08 -0600

    add sms-headless theme

---
+++ wp-content/themes/sms-headless/functions.php
@@ -0,0 +1,6 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+require_once('reboot/reboot.php');
+require_once('utils/post-duplicator.php');
+require_once('utils/post-order/post-order.php');
